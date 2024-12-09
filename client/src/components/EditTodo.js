0ffdd2b453effbfaaf2e14b0312e1f72c27c32c4
Aryan Padmakar Deshmukh <146423646+A-RYAN-1@users.noreply.github.com>
2024-12-09 23:42:11 +0530

    Changed URL

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -8,7 +8,7 @@ const EditTodo = ({ todo }) => {
     try {
       const body = { description };
       const response = await fetch(
-        `http://localhost:5000/todos/${todo.todo_id}`,
+        `https://todo-app-using-pern-stack-development.onrender.com/todos/${todo.todo_id}`,
         {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
